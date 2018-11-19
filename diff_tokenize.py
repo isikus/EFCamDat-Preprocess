10551ac22780777f77a7929948f0c7599be8c386
KellyHO <kelly192733@gmail.com>
2018-11-19 08:09:19 +0000

    update

--- diff_tokenize.py
+++ diff_tokenize.py
@@ -3,16 +3,21 @@ import os
 import re
 import fileinput
 import spacy
-
+import time
+from collections import defaultdict
 from nltk.tokenize import sent_tokenize, word_tokenize
 from spacy.lang.en import English
 from spacy.attrs import ORTH
+import kenlm
 
+
+KnModel = kenlm.Model('../jjc/1b.bin')
 SPACY_MODEL = os.environ.get('SPACY_MODEL', 'en')
 nlp = spacy.load(SPACY_MODEL)
 tokenizer = English().Defaults.create_tokenizer(nlp)
 
 edit_re = re.compile('\[\-(((?!\[\-).)*?)\-\]|\{\+(((?!\{\+).)*?)\+\}')
+sep_sents_re = r'\w\.\w'
 
 
 def init_tokenizer_option():
@@ -78,6 +83,41 @@ def mask_edits(text):
 
     return ' '.join(token.strip() for token in tokens), edits
 
+def seperate_sents(last_sent):
+    seg_sents = []
+    keep_seg = True
+    bef_sent = ''
+    while keep_seg:
+        
+        com_sent, last_sent = last_sent.split('.', 1)
+        com_sent = bef_sent + com_sent
+        com_sent += '.'
+    
+        if not last_sent: # prevent it is the last sent
+            seg_sents.append(reorganize_sent(com_sent))
+            break
+    
+        com_token = ' '.join(word_tokenize(com_sent))
+        last_token = ' '.join(word_tokenize(last_sent))
+        last_token = last_token.capitalize()
+        com_token = com_token.capitalize()
+        sep_scores = KnModel.score(com_token) + KnModel.score(last_token)
+        total_scores = KnModel.score(com_token + ' ' + last_token)
+    
+        if sep_scores > total_scores: # should seperate these sentences
+            bef_sent = ''
+            seg_sents.append(reorganize_sent(com_sent))
+
+        else:
+            bef_sent = com_sent
+        
+        if not re.search(r'\.', last_sent):
+            keep_seg = False
+            seg_sents.append(reorganize_sent(last_sent))
+    
+    return seg_sents
+def reorganize_sent(sent):
+    return ' '.join(token.text for token in tokenizer(sent) if token.text).strip()
 
 def tokenize_doc(text):
     text = restore_line_break(text)
@@ -91,21 +131,36 @@ def tokenize_doc(text):
     for line in text_masked.splitlines():
         # sentence tokenize (using nltk)
         for sent in sent_tokenize(line.strip()):
+            # print(sent)
+            # sent sep again (using nltk)
+            # if re.search(sep_sents_re, sent):
+                # tokenized_sents += seperate_sents(sent)
+            # else:
             # word tokenize (using spacy)
-            text = ' '.join(token.text for token in tokenizer(sent) if token.text).strip()
-            tokenized_sents.append(text)
-
+            # else:
+                # text = ' '.join(token.text for token in tokenizer(sent) if token.text).strip()
+                # tokenized_sents.append(reorganize_sent(sent))
+            tokenized_sents.append(sent)
     # restore masked edit
     return '\n'.join(tokenized_sents).format(*edits)
 
 
 def main():
+    revise_type = defaultdict(int)
     for doc in fileinput.input():
         doc = doc.strip()
         # print('#', 'doc', '=', doc)
         print(tokenize_doc(doc))
 
+    return revise_type
+
 
 if __name__ == '__main__':
+    start = time.time()
     init_tokenizer_option()
     main()
+    end = time.time()
+    #print('Time Cost: ', end - start)
+    #print('edit count')
+    #for key, count in revise_type.items():
+        #print('edit_count: ', key,'times: ', count)
